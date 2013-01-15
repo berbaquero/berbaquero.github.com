3ea0b625cb7d7a76b3606ab11187ab08e4b7d763
Bernardo Baquero Stand <baquerober@gmail.com>
2013-01-14 23:23:12 -0430

    New quick profile design
    Added links to my SoundCloud, LinkedIn and Twitter accounts.

---
+++ script.js
@@ -0,0 +1,34 @@
+var reposList = document.getElementById('repos-list'),
+	projectsInfo = document.getElementById('projects-info'),
+	load = function(data) {
+		if(!data || !data.data || !data.data.length) return;
+		var repositories = data.data,
+			html = '';
+		repositories.sort(function(a, b) {
+			var aFork = a.fork,
+				bFork = b.fork;
+			if(aFork && !bFork) return 1;
+			if(!aFork && bFork) return -1;
+			return new Date(b.pushed_at) - new Date(a.pushed_at);
+		});
+		var l = repositories.length,
+			lp = 0,
+			lf = 0,
+			w = 0,
+			f = 0;
+		for(var i = 0; i < l; i++) {
+			var r = repositories[i],
+				fork = r.fork ? ' class="fork"' : '',
+				watchers = r.watchers,
+				forks = r.forks;
+			w += r.watchers;
+			f += r.forks;
+			fork ? lf++ : lp++;
+			html += '<li' + fork + '>' + '<a href="' + r.homepage + '">' + '<span class="info"><b class="language">' + (r.language || '') + '</b> <b class="stars">' + watchers + '</b> <b class="forks">' + forks + '</b></span>' + '<b>' + r.name + '</b> ' + '<span class="desc">' + r.description + '</span>' + '</a>';
+		}
+		reposList.innerHTML = html;
+		projectsInfo.innerHTML = '<b class="stars">' + w + '</b> <b class="forks">' + f + '</b>';
+	};
+setTimeout(function() {
+	window.scrollTo(0, 1);
+}, 100);
